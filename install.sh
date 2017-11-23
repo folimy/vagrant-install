90ce8f5d170b387adccf8f810a9bc34585dd062d
Peter <folimworld@gmail.com>
2017-11-23 14:28:03 +0900

    add file address

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "centos vagrant 2.0.1 download "
+#wget https://releases.hashicorp.com/vagrant/2.0.1/vagrant_2.0.1_x86_64.rpm
 wget https://github.com/folimy/vagrant-install/raw/master/vagrant_2.0.1_x86_64.rpm
 
 echo "install vagrant"
